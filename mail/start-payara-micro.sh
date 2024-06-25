4505f6735ca5abffb812a49811d55c00a261b986
Michael P. Redlich <mike@redlich.net>
2024-06-25 10:10:55 -0400

    Updates 6/25/2024

--- mail/start-payara-micro.sh
+++ mail/start-payara-micro.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-java -jar /usr/local/bin/payara-micro/payara-micro-6.2024.4.jar target/mail-1.0.0.war
+java -jar /usr/local/bin/payara-micro/payara-micro-6.2024.6.jar target/mail-1.0.0.war
--- servlet/start-payara-micro.sh
+++ servlet/start-payara-micro.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-java -jar /usr/local/bin/payara-micro/payara-micro-6.2024.4.jar target/servlet-1.0.0.war
+java -jar /usr/local/bin/payara-micro/payara-micro-6.2024.6.jar target/servlet-1.0.0.war
