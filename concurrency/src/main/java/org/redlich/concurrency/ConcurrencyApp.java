e03c3eabce91ec5c2c976de3620ac7c387fd4078
Michael P. Redlich <mike@redlich.net>
2024-08-07 16:42:16 -0400

    Updates 8/7/2024

--- concurrency/src/main/java/org/redlich/concurrency/ConcurrencyApp.java
+++ concurrency/src/main/java/org/redlich/concurrency/ConcurrencyApp.java
@@ -20,6 +20,17 @@ public class ConcurrencyApp {
         ConcurrencyApp app = new ConcurrencyApp();
         String title = "[APP] Welcome to the Jakarta Concurrency Demo Application";
         app.displayTitle(title);
+        String payara = "[APP] Hosted on the Payara Platform and serverless Payara Cloud";
+        app.displayTitle(payara);
+        
+        System.out.println("[APP] Brought to you by Payara | Garden State JUG | Jakarta EE");
+        System.out.println("[APP] This is the Task Creator example application");
+        System.out.println();
+        System.out.println("[APP] Instructions:");
+        System.out.println("[APP] * Execute the command:");
+        System.out.println("[APP]      $mvn clean package && ./start-payara-micro.sh");
+        System.out.println("[APP] * In your browser, use the URL:");
+        System.out.println("[APP]      http://localhost:8080/concurrency-1.0.0");
         }
 
     /**
