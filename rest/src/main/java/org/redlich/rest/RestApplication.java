bcd1e4458ec48e6c6389c3751048f64689aa319c
Michael P. Redlich <mike@redlich.net>
2024-08-14 05:24:18 -0400

    Updates 8/13/2024

--- rest/src/main/java/org/redlich/rest/RestApplication.java
+++ rest/src/main/java/org/redlich/rest/RestApplication.java
@@ -11,6 +11,7 @@
  */
 package org.redlich.rest;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.GET;
@@ -28,6 +29,7 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
  *
  * @author airhacks.com
  */
+@ApplicationScoped
 @ApplicationPath("rest")
 @Produces(MediaType.APPLICATION_JSON)
 public class RestApplication extends Application {
--- rest/src/main/java/org/redlich/rest/RestResource.java
+++ rest/src/main/java/org/redlich/rest/RestResource.java
@@ -11,14 +11,20 @@
  */
 package org.redlich.rest;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import jakarta.ws.rs.core.UriBuilder;
+import jakarta.ws.rs.core.UriInfo;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+@ApplicationScoped
 @Path("rest")
 public class RestResource {
 
@@ -29,11 +35,15 @@ public class RestResource {
     @Inject
     RestService service;
 
+    @Inject
+    UriInfo uriInfo;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String rest() {
 
         StringBuilder builder = new StringBuilder();
+        builder.append(uriInfo.getMatchedResourceTemplate());
         builder.append(this.message);
         builder.append("\n");
         builder.append(service.message());
