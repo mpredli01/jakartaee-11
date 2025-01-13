abe5404b87b577ef5d641c0c65904fbd71792807
Michael P. Redlich <mike@redlich.net>
2025-01-13 16:49:18 -0500

    Updates 1/13/2025

--- jwtbridge/run.sh
+++ jwtbridge/run.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-curl -X GET http://localhost:8080/jwtbridge-1.0.0/api/secured/hi-with-token -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNaWtlIiwicm9sZXMiOlsiYWRtaW4iXSwiaXNzIjoicGF5YXJhIiwiZXhwIjoxNzM2NDYyNjA5fQ.M_1iAinHHTo19ruLpX0y9vf7B1HqlxsFr3KRjihALV4"
+curl -X GET http://localhost:8080/jwtbridge-1.0.0/api/secured/hi-with-token -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNaWtlIiwicm9sZXMiOlsiYWRtaW4iXSwiaXNzIjoicGF5YXJhIiwiZXhwIjoxNzM2ODkwMTAwfQ.5J_O40dmtPUCAH5MWgwXuiCV7HeTA7UfAFtsuS9dGDU"
 
