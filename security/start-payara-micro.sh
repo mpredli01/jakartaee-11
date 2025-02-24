3a163899ec61a70acc674da7979a3ddec56cd67f
Michael P. Redlich <mike@redlich.net>
2025-02-24 17:37:34 -0500

    Updates 2/24/2025

--- security/start-payara-micro.sh
+++ security/start-payara-micro.sh
@@ -1,5 +1,4 @@
 #!/bin/zsh
 
-# java -jar /usr/local/bin/payara-micro/payara-micro.jar target/security-1.0.0.war
-java -jar /usr/local/bin/payara-micro/payara-micro-6.2025.1.jar target/security-1.0.0.war
+java -jar /usr/local/bin/payara-micro/payara-micro-7.2025.1.Alpha1.jar target/security-1.0.0.war
 # java -Djakarta.security.jacc.PolicyFactory.provider="org.glassfish.exousia.modules.def.DefaultPolicyFactory" -jar /usr/local/bin/payara-micro/payara-micro-7.2024.1.Alpha2.jar target/security-1.0.0.war
