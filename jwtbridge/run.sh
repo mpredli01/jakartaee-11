9b5848745d317e809ecfc52a01b901ddeb44802e
Michael P. Redlich <mike@redlich.net>
2025-06-16 14:40:11 -0400

    Updates 6/16/2025

--- jwtbridge/run.sh
+++ jwtbridge/run.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-curl -X GET http://localhost:8080/jwtbridge-1.0.0/jwtbridge/secured/hi-with-token -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNaWtlIiwicm9sZXMiOlsiYWRtaW4iXSwiaXNzIjoicGF5YXJhIiwiZXhwIjoxNzQwNTgwOTg2fQ.Bt0HpVtCMJlVO9NF4NcUqa0iIsr9_ayDzKBjtn7n4N8"
+curl -X GET http://localhost:8080/jwtbridge-1.0.0/jwtbridge/secured/hi-with-token -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNaWtlIiwicm9sZXMiOlsiYWRtaW4iXSwiaXNzIjoicGF5YXJhIiwiZXhwIjoxNzUwMTc1OTA5fQ.TIlo3wGllr0DBJvKxhbmvPsIZPW2Z1sKEMFYHuvtbcQ"
 
--- servlet/start-payara-micro.sh
+++ servlet/start-payara-micro.sh
@@ -1,4 +1,3 @@
 #!/bin/zsh
 
-java -jar /usr/local/bin/payara-micro/payara-micro-7.2025.1.Alpha1.jar target/servlet-1.0.0.war
-# java -Djakarta.security.jacc.PolicyFactory.provider="org.glassfish.exousia.modules.def.DefaultPolicyFactory" -jar /usr/local/bin/payara-micro/payara-micro-7.2024.1.Alpha2.jar target/servlet-1.0.0.war
+java -jar /usr/local/bin/payara-micro/payara-micro-7.2025.1.Alpha2.jar target/servlet-1.0.0.war
