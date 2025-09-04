d2a649e6ca9472ad0cba792f1a3aeda7e06d32ce
Michael P. Redlich <mike@redlich.net>
2025-09-04 14:02:53 -0400

    Updates 9/4/2025

--- data/add.sh
+++ data/add.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # Add a brewer to the Brewer collection
-curl -X POST -H "Content-Type: application/json" -d '{"_id": 12, "name": "Redlich Brewing Company", "city": "Marco Island", "state": "Florida" }' http://localhost:8080/data-1.0.0/data/brewer/12
+curl -X POST -H "Content-Type: application/json" -d '{"_id": 8, "name": "Redlich Brewing Company", "city": "Berkshires", "state": "Florida" }' http://localhost:8080/data-1.0.0/data/brewer/8
 
 # Add a beer to the Beer collection
-curl -X POST -H "Content-Type: application/json" -d '{"_id": 8, "name": "Marco Island Stout", "type": "STOUT", "brewer_id": 8, "abv": 9.0}' http://localhost:8080/data-1.0.0/data/beer/8
+curl -X POST -H "Content-Type: application/json" -d '{"_id": 12, "name": "Berkshires Stout", "type": "STOUT", "brewer_id": 8, "abv": 9.0}' http://localhost:8080/data-1.0.0/data/beer/12
--- jwtbridge/run.sh
+++ jwtbridge/run.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-curl -X GET http://localhost:8080/jwtbridge-1.0.0/jwtbridge/secured/hi-with-token -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNaWtlIiwicm9sZXMiOlsiYWRtaW4iXSwiaXNzIjoicGF5YXJhIiwiZXhwIjoxNzU3MDgxNDYzfQ.MAxzE_h-uPpi4XxgPL1j_mrrjmn6kCI03FVZVxXURek"
+curl -X GET http://localhost:8080/jwtbridge-1.0.0/jwtbridge/secured/hi-with-token -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNaWtlIiwicm9sZXMiOlsiYWRtaW4iXSwiaXNzIjoicGF5YXJhIiwiZXhwIjoxNzU3MDkyNjA4fQ.4zbKyzeE3EVxlKJm4Bv4rSnd44oimXREjFV3xWgxHHE"
 
